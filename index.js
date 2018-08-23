95c817fd28e389e82761c87c869073e91c4b5e0a
Kurt <mrkurt@gmail.com>
2018-08-23 18:07:15 -0500

    Cache glitch domain lookup :fire:

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import db from "@fly/data"
 import proxy from "@fly/proxy"
+import cache from "@fly/cache"
 import hostnameAPI from "./src/api"
 
 const origin = proxy("https://example.glitch.me", { forwardHostHeader: true })
@@ -21,24 +22,49 @@ const fourOhFour = new Response("not found", { status: 404 })
 async function serveGlitchApp(req) {
   console.log("Serving glitch app")
   const h = req.headers.get("host")
-  if (!h) return fourOhFour
+  const key = `domain:${h}`
+  const lookupGlitchDomain = async (h) => {
+    if (!h) return
 
-  // check to see if we know this hostname
-  const record = await db.collection("hostnames").get(h)
-  if (!record || !record.app_id) {
-    console.error("hostname record not found:", h)
-    return fourOhFour
+    // check to see if we know this hostname
+    const record = await db.collection("hostnames").get(h)
+    if (!record || !record.app_id) {
+      console.error("hostname record not found:", h)
+      return
+    }
+
+    // get domain from Glitch API
+    const project = await getGlitchProject(record.app_id)
+    if (!project) {
+      console.error("Glitch project not found:", project)
+      return
+    }
+
+    const r = { domain: project.domain, time: Date.now() }
+    await cache.set(key, JSON.stringify(r))
+    return r
   }
 
-  // get domain from Glitch API
-  const project = await getGlitchProject(record.app_id)
-  if (!project) {
-    console.error("Glitch project not found:", project)
-    return fourOhFour
+  const cached = await cache.getString(key)
+  let record;
+
+  if (cached) {
+    try {
+      record = JSON.parse(cached)
+      if (Date.now() - record.time > 60000) {
+        lookupGlitchDomain(h) // recheck hostname in background
+      }
+    } catch (err) {
+      console.error("Error parsing JSON:", err)
+    }
   }
+
+  if (!record) record = await lookupGlitchDomain(h)
+  if (!record) return fourOhFour
 
   req.headers.set("X-Forwarded-Host", h)
-  req.headers.set("Host", `${project.domain}.glitch.me`)
+  req.headers.set("Host", `${record.domain}.glitch.me`)
+  console.log(`${h} -> ${record.domain}.glitch.me`)
 
   // do proxying
   return origin(req)
